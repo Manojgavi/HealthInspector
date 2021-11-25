4dadba49dfe46d816078afefd57edcf296e9790e
Manojkumar Gavireddygari <919314@cognizant.com>
2021-11-25 17:15:09 +0530

    Manoj 25112021 1714 changepassword

---
+++ HealthInspector/ViewModels/ForgotUserIdViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthInspector.ViewModels
+{
+    public class ForgotUserIdViewModel
+    {
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "What is your Pet Name?")]
+        public string Answer1 { get; set; }
+        [Required]
+        [Display(Name = "What is your First college name?")]
+        public string Answer2 { get; set; }
+        [Required]
+        [Display(Name = "What is your Best friend name?")]
+        public string Answer3 { get; set; }
+    }
+}
